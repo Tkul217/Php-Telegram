28fb65597574bf801efc1c064604f503e9765b3d
Timur <timurcool650@gmail.com>
2023-08-01 14:37:28 +0600

    Structure and autoload

--- Application/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Core\Route;
-
-require_once 'Core/model.php';
-require_once 'Core/view.php';
-require_once 'Core/controller.php';
-require_once 'Core/route.php';
-Route::start(); // запускаем маршрутизатор
