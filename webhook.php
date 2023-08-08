c38670a43860ff9c682751965ec7f7b5f5639322
Timur <timurcool650@gmail.com>
2023-08-08 16:57:31 +0600

    Sending a reply to user's message

--- webhook.php
+++ webhook.php
@@ -1,7 +1,16 @@
 <?php
+// Assuming you have an autoload file that includes the necessary classes
+require_once __DIR__ . '/vendor/autoload.php';
+
 $input = file_get_contents('php://input');
 
 $json = json_decode($input, true);
 
-file_put_contents('logger.txt', $input,
-    $json, FILE_APPEND);
+$telegram = new \App\Services\TelegramService();
+
+$chatId = $json['message']['chat']['id'];
+$messageText = 'Hello, this is your bot sending a message!';
+
+$response = file_get_contents($telegram->sendMessage($chatId, $messageText));
+
+file_put_contents('logger.txt', PHP_EOL . $response, FILE_APPEND);
